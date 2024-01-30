a2edd3f381d556c035dd837b4948f74200c1eeb0
Oneyker21 <oneygaleano21@gmail.com>
2024-01-29 19:17:08 -0600

    arreglo del slider pero con bug de animacion.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,32 @@ const seccionServicios = document.getElementById('servicios');
 const seccionImagenes = document.getElementById('imagenes');
 const seccionContacto = document.getElementById('contacto');
 
+document.addEventListener("DOMContentLoaded", function() {
+    var sliderNavLinks = document.querySelectorAll(".slider-nav a");
+
+    sliderNavLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+            
+            // Obtener el id del slider al que se hizo clic
+            var sliderId = this.getAttribute("href").substring(1);
+            
+            // Ocultar todas las imágenes del carrusel
+            var sliderImages = document.querySelectorAll(".sliderimg");
+            sliderImages.forEach(function(image) {
+                image.style.display = "none";
+            });
+            
+            // Mostrar la imagen correspondiente al id del slider
+            var targetImage = document.getElementById(sliderId);
+            if (targetImage) {
+                targetImage.style.display = "block";
+            }
+        });
+    });
+});
+
+
 btnInicio.addEventListener('click', () => {
     seccionInicio.style.display = 'block';
     seccionServicios.style.display = 'none';
